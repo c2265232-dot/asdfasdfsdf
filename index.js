b10f2f981c94f4cc803500d7149a732e1747c236
hxebdebs <c2265232@gmail.com>
2026-05-21 19:39:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
- let score = 0;
-function scoreChange() { score = score + 1; console.log(score) }
+let score = 0;
 
+const button = document.getElementById("btn");
+
+function scoreChange() {
+    score = score + 1;
+    console.log(score);
+}
+
+button.addEventListener("click", scoreChange);
